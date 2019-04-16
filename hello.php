d3c89eb5f970b1d921f9ff1633c3235858f55167
SOIP <38543876+sumofitsparts@users.noreply.github.com>
2019-04-16 11:26:20 -0400

    add html

--- hello.php
+++ hello.php
@@ -7,11 +7,21 @@ $result = $pi * $radius;
 $oxo = array(array('x', '', 'o'),
             array('o', 'o', 'x'),
             array('x', 'o', ''));
-echo <<<_END
-$hello
-<br>
-$oxo[1][2]
-<br>
-$pi times the $radius equals: $result
-_END
+$array = $oxo[1][2];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo $hello ?></title>
+</head>
+<body>
+    <h1><?php echo $hello ?></h1>
+    <p><?php echo "$pi times $radius equals: $result" ?><p>
+    <p><?php echo "The 3rd element in the 2nd array in the multidimentional array is $array"?></p>
+</body>
+</html>
+
